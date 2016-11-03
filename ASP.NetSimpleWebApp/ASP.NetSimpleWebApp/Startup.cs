5a2a091a07bc41b20119a3efa2bc0df14ccc60c0
Atul Malaviya <atulmal@microsoft.com>
2016-11-03 12:22:37 +0530

    first commit

---
+++ ASP.NetSimpleWebApp/ASP.NetSimpleWebApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ASP.NetSimpleWebApp.Startup))]
+namespace ASP.NetSimpleWebApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
